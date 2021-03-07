00460985458029848c49363893bd47bb45efb0bb
aniketgaikwad4635 <aniketgaikwad4635@gmail.com>
2021-03-07 18:04:20 +0530

    appointment import removed

--- src/main/java/com/example/demo/service/AppointmentService.java
+++ src/main/java/com/example/demo/service/AppointmentService.java
@@ -3,7 +3,7 @@ package com.example.demo.service;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Autowired; 
 import org.springframework.stereotype.Service;
 
 import com.example.demo.converter.AppointmentConverter;
